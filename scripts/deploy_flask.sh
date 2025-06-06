aca90be3416d90d800d84c9758db52321a2f4246
sivaram2662 <106672296+sivaram2662@users.noreply.github.com>
2025-06-06 18:33:53 +0530

    new

--- scripts/deploy_flask.sh
+++ scripts/deploy_flask.sh
@@ -11,7 +11,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return "Hello from Bastion EC2"
+    return "Hello Bastion"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
 EOF
